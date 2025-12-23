abda9e16f92a65201ba3df6515414fe68ba5d1d9
thibault <botigirard@gmail.com>
2025-12-23 20:11:23 +0100

    Format Monstre interface

--- Monstre.java
+++ Monstre.java
@@ -1,10 +1,12 @@
 
 public interface Monstre {
-    int getPV();
-    int getDegats();
-    void subirDegats(int degats);
-    void attaque(Monstre cible) throws MortException;
-    int getId();
+    int getPV();   
+    int getDegats() ;
+    void subirDegats(int degats);   
+    void attaque(Monstre cible) throws   MortException; 
+    int getId(); 
     String getNom();
-    void ajouteObservateur(Observateur o);
+  
+    void ajouteObservateur( Observateur o) ;
+
 }
