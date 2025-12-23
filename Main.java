81edd20e404136b6f7606af36269357707fd3a7f
thibault <botigirard@gmail.com>
2025-12-23 20:10:40 +0100

    Refactor Main.java for code formatting
    Formatted code for better readability.

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 public class Main {
+    
     public static void main(String[] args) {
         Arene arene = Arene.getInstance();
-        MonstreFactory f = new MonstreFactory();
-        Observateur terminal = new Terminal();
+        MonstreFactory f = new   MonstreFactory();
+        Observateur  terminal = new Terminal();
 
-        Monstre m1 = f.creerMonstre("Dragon avec une épée", 100, 10);
-        Monstre m2 = f.creerMonstre("Professeur avec un casque", 200, 20);
+        Monstre m1 = f.creerMonstre("Dragon avec une épée", 100, 10);   
+        Monstre  m2 = f.creerMonstre("Professeur avec un casque", 200, 20);
+
+        
         arene.ajouteMonstre(m1);
         arene.ajouteMonstre(m2);
 
-        arene.ajouteObservateurs(terminal);
+        arene.ajouteObservateurs(terminal) ;
+        
         arene.combat();
     }
+
 }
