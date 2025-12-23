b112b960312a25c5b20af22412b59d86660f5c21
thibault <botigirard@gmail.com>
2025-12-23 21:19:39 +0100

    Refactor MortException class formatting

--- MortException.java
+++ MortException.java
@@ -1,5 +1,8 @@
 
 public class MortException extends Exception {
     public MortException() { super("Le monstre est mort et ne peut pas attaquer."); }
-    public MortException(String msg) { super(msg); }
+    
+    public MortException(String msg) { 
+        super(msg); }
+
 }
