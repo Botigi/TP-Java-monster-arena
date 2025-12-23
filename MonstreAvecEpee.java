1b7c5ae642acb7ba0d712c57468e78e3fd178b89
thibault <botigirard@gmail.com>
2025-12-23 20:12:12 +0100

    Refactor MonstreAvecEpee class formatting

--- MonstreAvecEpee.java
+++ MonstreAvecEpee.java
@@ -1,15 +1,20 @@
 public class MonstreAvecEpee extends MonstreDecorateur {
-    public MonstreAvecEpee(Monstre inner) { super(inner); }
+    public MonstreAvecEpee(Monstre inner) { super(inner); 
+            }
 
     @Override
-    public int getDegats() { return inner.getDegats() * 2; }
+    public int getDegats() { return inner.getDegats() * 2; }  
 
+     
     
     @Override
     public void attaque(Monstre cible) throws MortException {
-        if (inner.getPV() <= 0) throw new MortException("Attaque impossible: mort.");
-        if (inner instanceof MonstreBase) ((MonstreBase) inner).notifie("attaque"); 
+        if (inner.getPV() <= 0) throw new MortException("Attaque impossible: mort.") ;
+        if (inner instanceof MonstreBase) {
+            ((MonstreBase) inner).notifie("attaque"); }
         cible.subirDegats(this.getDegats());
+    
     }
 
 }
+
