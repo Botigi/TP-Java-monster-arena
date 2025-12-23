feb9efbf6fb297768e526e1e0a6e87a61fdd3e4d
thibault <botigirard@gmail.com>
2025-12-23 21:21:15 +0100

    Refactor Terminal class formatting

--- Terminal.java
+++ Terminal.java
@@ -1,18 +1,23 @@
 public class Terminal implements Observateur {
+    
     @Override
     public void miseAJour(Monstre m, String evenement) {
 
         
         if (evenement.startsWith("subit:")) {
-            String dmg = evenement.substring("subit:".length());
-            System.out.println(m.getNom() + " (id " + m.getId() + ") subit " + dmg + " dégâts. PV restants: " + m.getPV());
+            
+            String dmg = evenement.substring("subit:".length()) ;
+            System.out.println(m.getNom() + " (id " + m.getId() + ") subit "+ dmg +" dégâts. PV restants: " + m.getPV());
         } else if (evenement.equals("attaque")) {
             System.out.println(m.getNom() + " (id " + m.getId() + ") attaque.");
-        } else if (evenement.equals("mort")) {
+            
+        } else if  (evenement.equals("mort")) {
             System.out.println(m.getNom() + " (id " + m.getId() + ") est mort.");
         } else {
-            System.out.println("[" + m.getNom() + " id " + m.getId() + "] " + evenement);
+            
+            System.out.println("[" + m.getNom() + " id " + m.getId() + "] " +  evenement );
         }
     }
 
 }
+
