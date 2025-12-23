e6f8ab2e19fa19ca31dccc71bf0ece7156d56fca
thibault <botigirard@gmail.com>
2025-12-23 20:13:32 +0100

    Refactor MonstreFactory class formatting

--- MonstreFactory.java
+++ MonstreFactory.java
@@ -2,26 +2,29 @@ public class MonstreFactory {
 
     public Monstre creerMonstre(String description, int pv, int degats) {
 
-                String desc = description.toLowerCase();
+               
+        String desc = description.toLowerCase();
 
         Monstre base;
         if (desc.contains("dragon")) {
             base = new Dragon(pv, degats);
+            
         } else if (desc.contains("professeur")) {
             base = new Professeur(pv, degats);
         } else {
 
-                    base = new Professeur(pv, degats);
-        }
+                    base = new Professeur(pv, degats);   }
 
         if (desc.contains("épée") || desc.contains("epee")) {
             base = new MonstreAvecEpee(base);
-        }
+        }   
         if (desc.contains("casque")) {
             base = new MonstreAvecCasque(base);
-        }
+        }  
 
         return base;
-    }
 
+        
+    }
 }
+
