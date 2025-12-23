afe28fb56a9a26d6fbb2e4c9fedaf61240c0e220
thibault <botigirard@gmail.com>
2025-12-23 20:06:34 +0100

    clean up this class

--- MonstreAvecEpee.java
+++ MonstreAvecEpee.java
@@ -4,12 +4,12 @@ public class MonstreAvecEpee extends MonstreDecorateur {
     @Override
     public int getDegats() { return inner.getDegats() * 2; }
 
-    // attaque() délègue mais utilise getDegats() de ce décorateur via cible.subirDegats(...)
+    
     @Override
     public void attaque(Monstre cible) throws MortException {
         if (inner.getPV() <= 0) throw new MortException("Attaque impossible: mort.");
-        // on notifie via inner (on garde cohérence des observateurs)
         if (inner instanceof MonstreBase) ((MonstreBase) inner).notifie("attaque"); 
         cible.subirDegats(this.getDegats());
     }
+
 }
